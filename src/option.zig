d12863f039d06eff5660d343122d05ee827d13fb
Nour AL KOTOB <alkotobnour@gmail.com>
2025-11-17 13:34:52 +0100

    add boolean and string validators

--- src/option.zig
+++ src/option.zig
@@ -4,36 +4,53 @@ const Arg = reentrant.Arg;
 const Flag = reentrant.Flag;
 const ReentrantArgIterator = reentrant.ReentrantArgIterator;
 const ParseIntError = std.fmt.ParseIntError;
+const ParseFloatError = std.fmt.ParseFloatError;
+
+fn ValidateBool(merge_fn: fn (?bool, bool) bool) type {
+    return struct {
+        fn validate(actual: ?bool, new: []const u8) !bool {
+            return merge_fn(actual, new.len > 0);
+        }
+    };
+}
+
+fn ValidateString(merge_fn: fn (?[]const u8, []const u8) []const u8) type {
+    return struct {
+        fn validate(actual: ?[]const u8, new: []const u8) ![]const u8 {
+            return merge_fn(actual, new);
+        }
+    };
+}
 
 fn ValidateInt(comptime T: type, base: u8, merge_fn: fn (?T, T) T) type {
     return struct {
-        fn validate(actual: ?T, new_value: []const u8) ParseIntError!T {
+        fn validate(actual: ?T, new: []const u8) ParseIntError!T {
             switch (@typeInfo(T)) {
                 .int => {},
-                else => @compileError("ValidateInt only validates integers."),
+                else => @compileError(@typeName(@This()) ++ " only validates " ++ @typeName(T)),
             }
-            return merge_fn(actual, try std.fmt.parseInt(T, new_value, base));
+            return merge_fn(actual, try std.fmt.parseInt(T, new, base));
         }
     };
 }
 
 fn ValidateFloat(comptime T: type, merge_fn: fn (?T, T) T) type {
     return struct {
-        fn validate(actual: ?T, new_value: []const u8) ParseIntError!T {
+        fn validate(actual: ?T, new: []const u8) ParseFloatError!T {
             switch (@typeInfo(T)) {
                 .float => {},
-                else => @compileError("ValidateInt only validates integers."),
+                else => @compileError(@typeName(@This()) ++ " only validates " ++ @typeName(T)),
             }
-            return merge_fn(actual, try std.fmt.parseFloat(T, new_value));
+            return merge_fn(actual, try std.fmt.parseFloat(T, new));
         }
     };
 }
 
 fn MergeReplace(comptime T: type) type {
     return struct {
-        pub fn merge(actual: ?T, new_value: T) T {
+        pub fn merge(actual: ?T, new: T) T {
             _ = actual;
-            return new_value;
+            return new;
         }
     };
 }
@@ -66,6 +83,33 @@ test "test" {
             },
         },
     );
+    const myStringMergingStrategy = MergeReplace([]const u8);
+    const myStringValidator = ValidateString(myStringMergingStrategy.merge);
+    var names3 = [_][]const u8{"my-chain"};
+    var cocoString = Option([]const u8){ .long_names = &names3, .validator = myStringValidator.validate };
+    _ = try cocoString.processArg(
+        &iterator,
+        Arg{
+            .long_flag = .{
+                .name = "my-chain",
+                .value = "tandoury",
+            },
+        },
+    );
+    const myBoolMergingStrategy = MergeReplace(bool);
+    const myBoolValidator = ValidateBool(myBoolMergingStrategy.merge);
+    var names4 = [_][]const u8{"my-bool"};
+    var cocoBool = Option(bool){ .long_names = &names4, .validator = myBoolValidator.validate };
+    _ = try cocoBool.processArg(
+        &iterator,
+        Arg{
+            .long_flag = .{
+                .name = "my-bool",
+                .value = "anything_non_null_will_result_in_true",
+            },
+        },
+    );
+    std.debug.print("\n", .{});
 }
 
 pub fn Option(comptime T: type) type {
@@ -74,7 +118,7 @@ pub fn Option(comptime T: type) type {
         long_names: ?[][]const u8 = null,
         description: ?[]const u8 = null,
         required: bool = false,
-        validator: *const fn (actual: ?T, new_value: []const u8) anyerror!T,
+        validator: *const fn (actual: ?T, new: []const u8) anyerror!T,
         value: ?T = null,
 
         pub fn processArg(self: *@This(), iterator: *ReentrantArgIterator, arg: Arg) !bool {
@@ -114,11 +158,17 @@ pub fn Option(comptime T: type) type {
                     self.value = try self.validator(self.value, value);
                 }
             }
-            std.debug.print("\nvalue is: {d}\n", .{self.value.?});
+            // TODO: test purpose, remove
+            switch (@TypeOf(self.value.?)) {
+                bool => {
+                    const label = if (value.len > 0) "true" else "false";
+                    std.debug.print("\nvalue is: {s}", .{label});
+                },
+                u8 => std.debug.print("\nvalue is: {d}", .{self.value.?}),
+                f64 => std.debug.print("\nvalue is: {e}", .{self.value.?}),
+                []const u8 => std.debug.print("\nvalue is: {s}", .{self.value.?}),
+                else => std.debug.print("\nvalue is (non-formatted): {any}", .{self.value.?}),
+            }
         }
     };
-}
-
-fn printArg(i: u8, arg: [:0]const u8) void {
-    std.log.debug("arg {d}\t\"{s}\"", .{ i, arg });
 }
