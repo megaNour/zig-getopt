e6106ab2ab2ee95c71b4a8a2ac768b44a0dc2c92
Nour AL KOTOB <alkotobnour@gmail.com>
2025-11-19 00:49:20 +0100

    working

--- src/main.zig
+++ src/main.zig
@@ -1,49 +1,45 @@
 const std = @import("std");
-const reentrant = @import("reentrant_arg_iterator.zig");
-const Option = @import("option.zig").Option;
-const Parser = @import("parser.zig").Parser;
+const option = @import("stateless_parser.zig");
+// const reentrant = @import("reentrant_arg_iterator.zig");
+// const Option = @import("option.zig").Option;
+// const Parser = @import("parser.zig").Parser;
 
-pub const std_options: std.Options = .{
-    .log_level = .debug,
-};
+// pub const std_options: std.Options = .{
+//     .log_level = .debug,
+// };
+//
+// const verbose_option: Option = .{
+//     .short_name = "v",
+//     .long_names = .{"verbose"},
+//     .description = "verbose stderr",
+// };
+// const dryrun_option: Option = .{
+//     .short_name = "d",
+//     .long_names = .{ "dryRun", "dry-run", "dryrun" },
+//     .description = "no op mode",
+// };
+//
+// fn iterateOverArgs(fn_ptr: fn (i: u8, [:0]const u8) void) void {
+//     var iterator = try std.process.argsWithAllocator(std.heap.page_allocator);
+//     defer iterator.deinit();
+//     var i: u8 = 0;
+//     _ = iterator.next();
+//     while (iterator.next()) |arg| : (i += 1) {
+//         fn_ptr(i, arg);
+//     }
+// }
 
-const verbose_option: Option = .{
-    .short_name = "v",
-    .long_names = .{"verbose"},
-    .description = "verbose stderr",
-};
-const dryrun_option: Option = .{
-    .short_name = "d",
-    .long_names = .{ "dryRun", "dry-run", "dryrun" },
-    .description = "no op mode",
-};
-
-fn iterateOverArgs(fn_ptr: fn (i: u8, [:0]const u8) void) void {
-    var iterator = try std.process.argsWithAllocator(std.heap.page_allocator);
-    defer iterator.deinit();
-    var i: u8 = 0;
-    _ = iterator.next();
-    while (iterator.next()) |arg| : (i += 1) {
-        fn_ptr(i, arg);
-    }
-}
-
-test "iterate over args" {
-    const coco = try reentrant.argsWithAllocator(std.heap.page_allocator, true);
-    _ = coco;
-}
+// test "iterate over args" {
+//     const coco = try reentrant.argsWithAllocator(std.heap.page_allocator, true);
+//     _ = coco;
+// }
 
 pub fn main() void {
     const iterator = try std.process.argsWithAllocator(std.heap.page_allocator);
-    var p: Parser = Parser.init(iterator);
-    while (p.nextShort('v')) |value| {
-        if (value.len > 0) {
-            std.debug.print("found flag with value: {s}\n", .{value});
-        } else {
-            std.debug.print("found flag with no value\n", .{});
-        }
-    } else {
-        std.debug.print("end of arguments reached\n", .{});
+    const longs = [_][]const u8{ "verbose", "ver" };
+    var myOpt = option.Option.init(iterator, 'v', &longs, option.Level.forbidden, "get useful debug output");
+    while (myOpt.next()) |cap| {
+        std.debug.print("found: {s}\n", .{cap});
     }
 }
 // fn matchOption(arg: Arg) void {
@@ -52,11 +48,11 @@ pub fn main() void {
 // //     }
 // // }
 
-var counter_max_positionals: u8 = 0;
-
-fn countMaxPositionals(_: u8, arg: [:0]const u8) void {
-    if (std.mem.eql(u8, "--", arg)) { // exception
-    } else if (arg.len > 1 and arg[0] == '-') return;
-    counter_max_positionals += 1;
-    std.debug.print("arg: {s}\tmax possible positionals is now: {d}\n", .{ arg, counter_max_positionals });
-}
+// var counter_max_positionals: u8 = 0;
+//
+// fn countMaxPositionals(_: u8, arg: [:0]const u8) void {
+//     if (std.mem.eql(u8, "--", arg)) { // exception
+//     } else if (arg.len > 1 and arg[0] == '-') return;
+//     counter_max_positionals += 1;
+//     std.debug.print("arg: {s}\tmax possible positionals is now: {d}\n", .{ arg, counter_max_positionals });
+// }
---
+++ src/stateless_parser.zig
@@ -0,0 +1,138 @@
+const std = @import("std");
+const ArgIterator = std.process.ArgIterator;
+
+const ParsingError = error{
+    MissingValue,
+    ForbiddenValue,
+    ForbiddenFlagPosition,
+    ForbiddenEqualPosition,
+};
+
+pub const Positional = struct {
+    iter: ArgIterator,
+
+    pub fn init(iter: ArgIterator) Positional {
+        return Positional{
+            .iter = iter,
+        };
+    }
+
+    pub fn next(self: *Positional) ?[]const u8 {
+        while (self.iter.next()) |arg| {
+            if (arg.len < 2) {
+                return arg;
+            } else if (arg[0] != '-') {
+                return arg;
+            } else if (arg.len == 2 and std.mem.eql(u8, arg, "--")) {
+                return null;
+            } // continue
+        } else return null;
+    }
+};
+
+pub const Level = enum(u2) {
+    forbidden,
+    allowed,
+    required,
+};
+
+pub const Option = struct {
+    iter: ArgIterator,
+    short: ?u8,
+    longs: ?[]const []const u8,
+    req_lvl: Level = .forbidden,
+    desc: ?[]const u8,
+
+    pub fn init(
+        arg_iterator: ArgIterator,
+        short_name: ?u8,
+        long_names: ?[]const []const u8,
+        value_requirement_level: Level,
+        description: ?[]const u8,
+    ) Option {
+        return Option{
+            .iter = arg_iterator,
+            .short = short_name,
+            .longs = long_names,
+            .req_lvl = value_requirement_level,
+            .desc = description,
+        };
+    }
+
+    pub fn next(self: *Option) ?[]const u8 {
+        while (self.iter.next()) |arg| {
+            if (arg.len < 2) {
+                continue; // positional
+            } else if (arg[0] == '-') { // option check needed
+                if (arg[1] == '-') { // longs or term
+                    if (arg.len == 2) {
+                        return null;
+                    } else {
+                        if (self.longs) |longs| {
+                            for (longs) |l| {
+                                if (std.mem.eql(u8, arg[2..], l)) {
+                                    return self.parseArg(arg[2..]);
+                                }
+                            }
+                        }
+                        continue;
+                    }
+                } else { // flag chain
+                    for (arg[1..], 1..) |c, i| {
+                        if (c == '=') {
+                            std.debug.panic("{}: '=' can only precede a value after a valid flag name. Got: {s}", .{ ParsingError.ForbiddenEqualPosition, arg });
+                        } else if (c != self.short) {
+                            continue;
+                        } else switch (self.req_lvl) {
+                            Level.required => {
+                                if (arg.len > i + 1) {
+                                    if (arg[i + 1] == '=') {
+                                        return self.parseArg(arg[i..]);
+                                    } else if (arg[i + 1] != '=') {
+                                        std.debug.panic("{}: '{d}' requires an argument. It can be alone or the last one of a flag chain. Got: {s}", .{ ParsingError.MissingValue, c, arg });
+                                    }
+                                } else std.debug.panic("{}: '{d}' requires an argument. It can be alone or the last one of a flag chain. Got: {s}", .{ ParsingError.MissingValue, c, arg });
+                            },
+                            Level.allowed => {
+                                if (arg.len > i + 1) {
+                                    if (arg[i + 1] == '=') {
+                                        return self.parseArg(arg[i..]);
+                                    } else if (arg[i + 1] != '=') {
+                                        std.debug.panic("{}: '{d}' can take an argument. It can be alone or the last one of a flag chain. Got: {s}", .{ ParsingError.MissingValue, c, arg });
+                                    }
+                                } else return self.parseArg(arg[i..]);
+                            },
+                            Level.forbidden => {
+                                return self.parseArg(arg[i..]);
+                            },
+                        }
+                    }
+                }
+            }
+        } else return null;
+    }
+
+    /// Here we consider we don't receive empty args.
+    fn parseArg(self: *Option, arg: []const u8) ?[]const u8 {
+        switch (self.req_lvl) {
+            Level.forbidden => {
+                return if (std.mem.indexOfScalarPos(u8, arg, 1, '=')) |pos| std.debug.panic("{}: {s} cannot take an argument. Got: {s}", .{ ParsingError.ForbiddenValue, arg[0..pos], arg }) else "";
+            },
+            Level.allowed => {
+                if (std.mem.indexOfScalarPos(u8, arg, 1, '=')) |i| {
+                    return if (arg[i + 1 ..].len > 0) arg[i + 1 ..] else std.debug.panic("{}: {s} was given with an '=' but no value was provided", .{ ParsingError.MissingValue, arg[0..i] });
+                } else return "";
+            },
+            Level.required => {
+                if (std.mem.indexOfScalarPos(u8, arg, 1, '=')) |i| {
+                    return if (arg[i + 1 ..].len > 0) arg[i + 1 ..] else std.debug.panic("{}: {s} requires a value, but none was provided.", .{ ParsingError.MissingValue, arg[0..i] });
+                } else return std.debug.panic("{}: {s} requires a value, but none was provided.", .{ ParsingError.MissingValue, arg });
+            },
+        }
+    }
+};
+
+const Command = struct {
+    name: []const u8,
+    ptr: *const fn (iter: ArgIterator) void,
+};
