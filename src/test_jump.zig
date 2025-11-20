4b74693fa2ff16ac75e24e2a2730bea4499c24ee
Nour AL KOTOB <alkotobnour@gmail.com>
2025-11-20 17:39:57 +0100

    start to write real exhaustive tests

--- src/test_jump.zig
+++ src/test_jump.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const ArgIterator = std.process.ArgIterator;
 const jump = @import("jump.zig");
+const expect = std.testing.expect;
 
 pub const StringIterator = struct {
     stock: []const [:0]const u8,
@@ -16,12 +17,27 @@ pub const StringIterator = struct {
     }
 };
 
-test {
-    const iterator = StringIterator{ .stock = &.{ "-v", "--verbose" } };
+test "jump flags with forbidden value affectation" {
+    const iterator = StringIterator{ .stock = &.{ "--verbose", "alif", "--ba", "-ta", "-vv", "-av", "--", "-vvv" } };
+    //                                             |   1                                2       1    --      3|
+    //                                             |————————————————————————— 4 —————————————————————|—— 3 ———|
     var jumper = jump.Over(StringIterator).init(iterator, 'v', &.{"verbose"}, .forbidden, "verbose logs");
-    var aggregator = jumper;
-    while (jumper.next()) |val| {
-        std.debug.print("found: {d} verbose flag!", .{val[0]});
-    }
-    std.debug.print("total verbose flags: {d}", .{aggregator.count()});
+    var copyOfJumper = jumper; // start from the same point
+    try expect(4 == copyOfJumper.count());
+    try expect(1 == jumper.next().?[0]);
+    try expect(2 == jumper.next().?[0]);
+    try expect(1 == jumper.next().?[0]);
+    try expect(null == jumper.next()); // unlike next(), count() already hit it and swallowed it
+    try expect(3 == jumper.next().?[0]);
+    try expect(3 == copyOfJumper.count());
+}
+
+test "jump flags with wrong value affectation" {
+    const iterator = StringIterator{ .stock = &.{ "--verbose=a", "alif", "--ba", "-ta", "-vv=", "-av", "--", "-v=vv" } };
+    var jumper = jump.Over(StringIterator).init(iterator, 'v', &.{"verbose"}, .forbidden, "verbose logs");
+    std.debug.print("{any}", .{jumper.next().?});
+    // try expect(2 == jumper.next().?[0]);
+    // try expect(1 == jumper.next().?[0]);
+    // try expect(null == jumper.next());
+    // try expect(3 == jumper.next().?[0]);
 }
